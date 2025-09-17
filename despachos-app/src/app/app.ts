399c279d71d49fabdf59a962c46c9ef58f323ccd
Anabella Jaua <j.anabella@correo.unimet.edu.ve>
2025-09-17 12:02:41 -0400

    💄 Estilos iniciales: header y footer

--- despachos-app/src/app/app.ts
+++ despachos-app/src/app/app.ts
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('despachos-app');
+ 
+  toggleMenu() {
+    const nav = document.querySelector('.nav-links');
+    nav?.classList.toggle('show');
+  }
+  
 }
