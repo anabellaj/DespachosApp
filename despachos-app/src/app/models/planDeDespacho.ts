72b1cf35fbffd1dd35f9695c9d03313ad1fff2c6
Anabella Jaua <j.anabella@correo.unimet.edu.ve>
2025-09-17 15:49:16 -0400

    🗃️ Clases principales

--- despachos-app/src/app/models/planDeDespacho.ts
+++ despachos-app/src/app/models/planDeDespacho.ts
@@ -1,12 +1,50 @@
+import { Camion } from "./camion";
+import { Chofer } from "./chofer";
+import { Pedido } from "./pedido";
+
 export class PlanDeDespacho {
 
-    /* Inicializador de la clase con los atributos de id, placa y status */
+    // El estatus inicial siempre es 'Planificado'
+    public estatus: 'Planificado' | 'Cargando' | 'En Ruta' | 'Completado' = 'Planificado';
+
+    /**
+     * Metodo constructor de la clase PlanDeDespacho
+     * @param id 
+     * @param nombreRuta 
+     * @param camion 
+     * @param chofer 
+     * @param pedidos 
+     */
     constructor(
         public id: number,
-        public nombreTienda: string,
-        public numeroItems: number,
-        public destino: string
+        public nombreRuta: string,
+        public camion: Camion,
+        public chofer: Chofer,
+        public pedidos: Pedido[]
     ) {}
 
+    /**
+     * Metodo para iniciar la carga del camion, cambiando el estatus a 'Cargando'
+     */
+    iniciarCarga() {
+        this.estatus = 'Cargando';
+    }
+
+    /**
+     * Metodo para indicar que el camion ha salido a ruta, cambiando el estatus a 'En Ruta'
+     */
+    salirARuta() {
+        this.estatus = 'En Ruta';
+    }
+
+    /**
+     * Metodo para completar el plan de despacho, cambiando el estatus a 'Completado' y liberando el camion y chofer asignados
+     */
+    completar() {
+        this.estatus = 'Completado';
+        this.camion.liberar();   
+        this.chofer.liberar();   
+    }
 
 }
+    
